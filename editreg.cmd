e2de46d2be6f3df033a3036b442a88f12ec5d7bf
Matt Dunn <mattd@saucelabs.com>
2019-01-16 17:18:44 +0000

    Update editreg.cmd

--- editreg.cmd
+++ editreg.cmd
@@ -2,7 +2,8 @@
 echo starting prerun
 echo Windows Registry Editor Version 5.00 > mediaCapture.reg
 echo. >> mediaCapture.reg
-echo [HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\MediaCapture\AllowDomains] "https://www.onlinemictest.com"=dword:00000002 >> mediaCapture.reg
+echo [HKEY_CURRENT_USER\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\MediaCapture\AllowDomains] >> mediaCapture.reg
+echo "https://www.onlinemictest.com"=dword:00000002 >> mediaCapture.reg
 echo content of registry file to import:
 type mediaCapture.reg
 echo importing registry file
